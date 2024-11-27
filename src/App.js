a022a59638a052ad0a004e6a4f4613acc3178fd3
vuphucky <phucky1235@gmail.com>
2024-11-27 09:46:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddBook from "./components/AddBook";
+import EditBook from "./components/EditBook";
+import Library from "./components/Library";
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/edit/:id" element={<EditBook />} />
+        </Routes>
+      </Router>
   );
 }
 
